cead6751e9f74eac6491a9b3599d49b04037547b
= <=>
2025-04-14 12:26:28 +0100

    feat: Add data contact

---
+++ data/data.py
@@ -0,0 +1,32 @@
+contact = [
+    {
+        "name": "John Doe",
+        "number": 990312922,
+        "email": "jondoe@gmail.com"
+    },
+    {
+        "name": "Amina Kabamba",
+        "number": 991234567,
+        "email": "amina.kabamba@exemple.cd"
+    },
+    {
+        "name": "Patrick Tshibanda",
+        "number": 972345678,
+        "email": "patrick.tshi@exemple.cd"
+    },
+    {
+        "name": "Bénédicte Mwamba",
+        "number": 977889900,
+        "email": "benedicte.mw@exemple.cd"
+    },
+    {
+        "name": "Serge Mulumba",
+        "number": 976001122,
+        "email": "serge.mulumba@exemple.cd"
+    },
+    {
+        "name": "Christelle Kiala",
+        "number": 974558899,
+        "email": "christelle.kiala@exemple.cd"
+    }
+]
